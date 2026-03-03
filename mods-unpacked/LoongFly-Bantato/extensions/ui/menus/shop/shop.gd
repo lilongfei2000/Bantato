160900e930a35cc8f4a87be57c790242fe2c3794
lilongfei <lilongfeichina@outlook.com>
2026-03-03 14:07:09 +0800

    refactor(shop): rename the function to align with base_shop

--- mods-unpacked/LoongFly-Bantato/extensions/ui/menus/shop/shop.gd
+++ mods-unpacked/LoongFly-Bantato/extensions/ui/menus/shop/shop.gd
@@ -1,5 +1,5 @@
 extends "res://ui/menus/shop/shop.gd"
 
 
-func _on_banned_element_focused(element: InventoryElement, player_index: int) -> void:
+func _bantato_on_element_focused(element: InventoryElement, player_index: int) -> void:
     _on_element_focused(element, player_index)
